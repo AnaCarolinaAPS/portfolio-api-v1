91b7e4a3b8718f70ca9f20d41e629f9c08501922
Ana Carolina <anacarolinaaps@gmail.com>
2026-01-24 18:54:15 -0300

    [feature] (Profile) Create ProfileObserver
    - Create ProfileObserver, when a new profile is created we create a Information for its provider;
    - Add ProfileObserver on AppServiceProvider;

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Profile;
 use App\Models\User;
+use App\Observers\ProfileObserver;
 use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         User::observe(UserObserver::class);
+        Profile::observe(ProfileObserver::class);
     }
 }
