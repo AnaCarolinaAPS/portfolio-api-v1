605c8a5daada0a6ed5d76581047caa9642ddc776
Ana Carolina <anacarolinaaps@gmail.com>
2026-01-24 19:08:41 -0300

    [bugfix] (Information) Navigation View Active
    - Changed :active="request()->routeIs('information.index')" to :active="request()->routeIs('informations.index')"

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('informations.index')" :active="request()->routeIs('information.index')">
+                    <x-nav-link :href="route('informations.index')" :active="request()->routeIs('informations.index')">
                         {{ __('Information') }}
                     </x-nav-link>
                 </div>
